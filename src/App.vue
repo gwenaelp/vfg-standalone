1fdbe7f0c0c4d2963b1efc94435608ba8ee4e449
Gwenael Pluchon <accounts@gwenp.fr>
2024-06-19 18:50:14 +0200

    first proto

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { emitter, formSchema, formModel } from './main.ts';
+
+const submit = () => {
+  emitter.emit('submit', formModel.value);
+}
+</script>
+
+<template>
+  <vue-form-generator
+    :schema="formSchema"
+    :model="formModel"
+  />
+  <button @click="submit">
+    Apply
+  </button>
+</template>
+<style scoped>
+:deep(fieldset) {
+  border: 0;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import { createApp, ref, } from 'vue'
+import './style.css'
+import App from './App.vue'
+import VueFormGenerator from '@dashy-soft/vue-form-generator/src/index';
+import Emitter from 'tiny-emitter';
+import '@dashy-soft/vue-form-generator/dist/vfg.css';
+
+const emitter = new Emitter();
+let formSchema = ref({});
+let formModel = ref({});
+
+export default ({
+  schema,
+  model,
+  selector,
+  onSubmit,
+}) => {
+
+  formSchema.value = schema;
+  formModel.value = model || {};
+  const app = createApp(App);
+	app.component('field-input', VueFormGenerator.fieldsLoader.fieldInput);
+	app.component('field-checklist', VueFormGenerator.fieldsLoader.fieldChecklist);
+	app.component('field-dateTimePicker', VueFormGenerator.fieldsLoader.fieldDateTimePicker);
+	app.component('field-label', VueFormGenerator.fieldsLoader.fieldLabel);
+	app.component('field-radios', VueFormGenerator.fieldsLoader.fieldRadios);
+
+	app.use(VueFormGenerator);
+	app.mount(selector || '#app');
+
+  emitter.on('submit', () => {
+    if(onSubmit) {
+      onSubmit(formModel.value);
+    }
+  });
+
+  return {
+  };
+};
+
+export { emitter, formSchema, formModel };
